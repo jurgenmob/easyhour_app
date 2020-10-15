ce41e3edb9d88f47dd36c747096541684a021499
Maurizio Pinotti <maurizio@easyhour.app>
2020-10-15 18:58:09 +0200

    Added crashlytics and push notifications on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +11,8 @@ import GoogleMaps
   ) -> Bool {
     GMSServices.provideAPIKey(Constants.GOOGLE_MAPS_API_KEY)
     GeneratedPluginRegistrant.register(with: self)
+    if (FirebaseApp.app() == nil) { FirebaseApp.configure() }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
